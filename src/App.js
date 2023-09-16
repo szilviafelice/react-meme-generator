55e2933be0846f2fbc131243f4b0a2b467792f8b
szilviafelice <szilvia.felice@gmail.com>
2023-09-16 04:13:53 -0400

    Doge meme appearing.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import { useState } from 'react';
 
 export default function App() {
-  const [button, setButton] = useState('');
-  const [text, setText] = useState('');
+  const [text, setText] = useState('Welcome');
   const [text2, setText2] = useState('');
-  const [meme, setMeme] = useState('');
+  const [meme, setMeme] = useState('doge');
+
+  const url = `https://api.memegen.link/images/${meme}/${text}/${text2}.jpg`;
+
+  console.log({ url });
+
+  //const  = () => (    )
 
   return (
     <div>
+      <img src={url} alt="Meme picture" data-test-id="meme-image"></img>
       <div>
-        <h2>Top text:</h2>
+        <label htmlFor="topText">Top text:</label>
         <h3>{text}</h3>
+
         <input
+          id="topText"
           onChange={(event) => setText(event.currentTarget.value)}
           value={text}
         />
-        <h2>Bottom text:</h2>
+        <label htmlFor="bottomText">Bottom text:</label>
         <h3>{text2}</h3>
         <input
+          id="bottomText"
           onChange={(event) => setText2(event.currentTarget.value)}
           value={text2}
         />
       </div>
-      <img src="" alt="Meme picture" data-test-id="meme-image"></img>
-
       <div>
-        <label htmlFor="memeTemplate">Meme template: </label>
-        <input type="text" id="memeTemplate" placeholder="Enter meme name" />
+        <span
+          role="button"
+          tabIndex={0}
+          onKeyPress={() => {}}
+          onClick={() => setMeme('')}
+        >
+          Meme template:
+        </span>
+        <input
+          id="memeTemplate"
+          placeholder="Enter meme name"
+          onChange={(event) => setMeme(event.currentTarget.value)}
+          value={meme}
+        />
       </div>
-
-      {meme}
-      <button
-        onClick={() => setMeme('https://api.memegen.link/images')}
+      <a
+        href={url}
+        title="Download meme"
+        download="meme.jpg"
         style={{
           backgroundColor: 'black',
           color: 'pink',
@@ -39,7 +58,7 @@ export default function App() {
         }}
       >
         Download
-      </button>
+      </a>
     </div>
   );
 }
